1519f6fffb5f8d2bd06cf094d7e89d690e057b9a
Themi <tcm4760@gmail.com>
2022-08-25 21:51:55 -0500

    Added timer to test how long it takes to read mca file.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import mcaReader
+import time
 
 with open("in/r.0.0.mca", "rb") as f:
     data = f.read()
 
+start = time.time()
+
 mca = mcaReader.MCA(data)
+
+end = time.time()
 # All chunks in mca file are read, but for demonstration purposes I'm only printing out the first one.
 print(mca.chunks[0].nbt_data)
+print(f"Time taken: {end-start}")
